e2bd22fa7bd082007dcfcbf5667df788d8a1b411
KTakundwa <st10471537@vcconnect.edu.za>
2025-09-04 14:52:01 +0200

    I added a heading, changed the styling of the response, added an underline to the heading, an online image, margins and alignment.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
 
 export default function App() {
+  const age = 18;
+  const yearsexperience = 5;
+  let feedback = '';
+  
+  if (age >= 18) 
+    if (yearsexperience >= 3)
+    {
+      feedback= 'you are a qualified, safe driver'
+
+    }
+    else
+    {
+    feedback = 'You are allowed to drive.  YAY!'
+    } else
+    {
+      feedback= 'You are not allowed to drive yet'
+    }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.container}>
+    <View style= {styles.content}>
+    <Text  style={[styles.response, {fontSize: 48, marginBottom: 5, color: 'black', textAlign: 'center', textDecorationLine: 'underline'}]}>Drive To survive
+             
+             </Text>
+       <Image source={{uri:"https://i1.pickpik.com/photos/212/341/296/car-girl-driving-transportation-preview.jpg"}} 
+          resizeMode="contain"
+          style= {styles.image}/>
+            <Text  style={styles.response}>Age: {age}
+             
+            </Text>
+            <Text  style={styles.response}>License Years {yearsexperience }
+             
+             </Text>
+
+          <Text style={[styles.response, {color: 'green', fontWeight: 'bold'}] }>
+      {feedback} {/*This is my message to be displayed to the user */}
+      
+      </Text>
     </View>
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  content:{
+    width:'100%',
+    alignItems: 'center'
+
+  },
+
+  response: {
+    fontSize: 28,
+    color: 'black',
+    fontWeight: 'bold',
+    marginVertical: 5
+
+  },
+  image :{
+    width: '100%',
+    height: '50%',
+    resizeMode: 'cover',
+    top: 0
+
+  }
 });
